a3a5dcd77d7e94f7a114542ae28e6962b693f76f
abdukodir <abdukodirsheraliev1@gmail.com>
2026-01-25 19:54:39 +0900

    feat: develop My Bookings Page

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import { Toaster } from "react-hot-toast";
 import AuthToasts from "./components/AuthToasts";
 import AllRooms from "./pages/AllRooms";
 import RoomDetails from "./pages/RoomDetails";
+import MyBookings from "./pages/MyBookings";
 
 const App = () => {
   const isOwnerPath = useLocation().pathname.includes("owner");
@@ -32,9 +33,9 @@ const App = () => {
           <Route path="/" element={<Home />} />
           <Route path="/rooms" element={<AllRooms />} />
           <Route path="/rooms/:id" element={<RoomDetails />} />
+          <Route path="/my-bookings" element={<MyBookings />} />
         </Routes>
       </div>
-
       <Footer />
     </div>
   );
