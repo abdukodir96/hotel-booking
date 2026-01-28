0d52f8648a4612417239bd117b42e7be6512b719
abdukodir <abdukodirsheraliev1@gmail.com>
2026-01-29 00:53:09 +0900

    fix: modify HotelCard responsive functionality

--- src/components/FeaturedDestination.jsx
+++ src/components/FeaturedDestination.jsx
@@ -1,30 +1,31 @@
 import React from "react";
-import { roomsDummyData } from "../assets/assets";
 import HotelCard from "./HotelCard";
 import Title from "./Title";
-import { useNavigate } from "react-router-dom";
+import { useAppContext } from "../context/AppContext";
 
 const FeaturedDestination = () => {
-  const navigate = useNavigate();
+  const { rooms, navigate } = useAppContext();
+
   return (
-    <div className="flex flex-col items-center px-6 md:px-16 lg:px-24 bg-slate-50 py-20">
-      <Title
-        title="Featured Destinations"
-        subTitle="A refined selection of world-class destinations where comfort, design, and experience come together."
-      />
+    rooms.length > 0 && (
+      <div className="flex flex-col items-center px-6 md:px-16 lg:px-24 bg-slate-50 py-20">
+        <Title
+          title="Featured Destinations"
+          subTitle="A refined selection of world-class destinations where comfort, design, and experience come together."
+        />
 
-      <div className="flex flex-wrap items-center justify-center gap-6 mt-20">
-        {roomsDummyData.slice(0, 4).map((room, index) => (
-          <HotelCard key={room._id} room={room} index={index} />
-        ))}
-      </div>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 mt-20 w-full max-w-6xl">
+          {rooms.slice(0, 4).map((room, index) => (
+            <HotelCard key={room._id} room={room} index={index} />
+          ))}
+        </div>
 
-      <button
-        onClick={() => {
-          navigate("/rooms");
-          scrollTo(0, 0);
-        }}
-        className="
+        <button
+          onClick={() => {
+            navigate("/rooms");
+            scrollTo(0, 0);
+          }}
+          className="
     my-16 px-6 py-2 text-sm font-medium
     border border-gray-300 rounded
     bg-white text-gray-800
@@ -34,10 +35,11 @@ const FeaturedDestination = () => {
     hover:-translate-y-0.5
     cursor-pointer
   "
-      >
-        View All Destinations
-      </button>
-    </div>
+        >
+          View All Destinations
+        </button>
+      </div>
+    )
   );
 };
 
